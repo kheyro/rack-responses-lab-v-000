ef7cc167acca8f1d1f2ab46693fbf72116dc768f
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-08 18:34:13 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,11 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    t = Time.now
+    msg = (t.hour > 12) ? "Good afternoon" : "Good morning"
+    resp.write(msg)
+    
+    resp.finish
+end
